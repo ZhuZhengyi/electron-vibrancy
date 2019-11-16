aa169f8a17835a71ca3dae843e6083066355e87c
Justice <justice_103@126.com>
2019-11-16 18:02:37 +0800

    update

--- src/vibrancy_mac.cc
+++ src/vibrancy_mac.cc
@@ -152,7 +152,7 @@ namespace Vibrancy {
         if (vView->IsNull() || !vView->IsInt32())
             return result;
 
-        int viewId = vView->Int32Value(v8::Isolate::GetCurrent());
+        int viewId = vView->Int32Value(Nan::GetCurrentContext());
 
         if (viewId == -1 || viewId > static_cast<int>(views_.size()))
             return result;
@@ -200,11 +200,11 @@ namespace Vibrancy {
             v8::String::NewFromUtf8(isolate, "Material"));
 
         if (!vMaterial->IsNull() && vMaterial->IsInt32()) {
-            viewOptions.Material = vMaterial->Int32Value(isolate);
+            viewOptions.Material = vMaterial->Int32Value(Nan::GetCurrentContext());
         }
 
         if (!vViewId->IsNull() && vViewId->IsInt32())
-            viewOptions.ViewId = vViewId->Int32Value(isolate);
+            viewOptions.ViewId = vViewId->Int32Value(Nan::GetCurrentContext());
 
         if (!vSize->IsUndefined() && !vSize->IsNull()) {
             V8Array vaSize =
@@ -216,10 +216,10 @@ namespace Vibrancy {
                 vaSize->Get(v8::String::NewFromUtf8(isolate, "height"));
 
             if (!vWidth->IsNull() && vWidth->IsInt32())
-                viewOptions.Width = vWidth->Int32Value(isolate);
+                viewOptions.Width = vWidth->Int32Value(Nan::GetCurrentContext());
 
             if (!vHeight->IsNull() && vHeight->IsInt32())
-                viewOptions.Height = vHeight->Int32Value(isolate);
+                viewOptions.Height = vHeight->Int32Value(Nan::GetCurrentContext());
         }
 
         if (!vPosition->IsUndefined() && !vPosition->IsNull()) {
@@ -229,14 +229,14 @@ namespace Vibrancy {
             V8Value vY = vaPosition->Get(v8::String::NewFromUtf8(isolate, "y"));
 
             if (!vX->IsNull() && vX->IsInt32())
-                viewOptions.X = vX->Int32Value(isolate);
+                viewOptions.X = vX->Int32Value(Nan::GetCurrentContext());
 
             if (!vY->IsNull() && vY->IsInt32())
-                viewOptions.Y = vY->Int32Value(isolate);
+                viewOptions.Y = vY->Int32Value(Nan::GetCurrentContext());
         }
 
         if (!vAutoResizeMask->IsNull() && vAutoResizeMask->IsInt32()) {
-            viewOptions.ResizeMask = vAutoResizeMask->Int32Value(isolate);
+            viewOptions.ResizeMask = vAutoResizeMask->Int32Value(Nan::GetCurrentContext());
         }
 
         return viewOptions;
